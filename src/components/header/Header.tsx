acd6ad2635d2a81d17867883d5d11dfddfa342a7
Ebenitez03 <elianenbenitezd04@gmail.com>
2024-12-03 08:48:28 +0100

    feat: Header

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,17 @@
+import Hamburguer_menu from "./hamburguer_menu/Hamburguer_menu";
+import styles from "./Header.module.scss";
+import Title_box from "./title_box/Title_box";
+import Btn_reusable from "../btn_reusable/Btn_reusable";
+function Header() {
+  return (
+    <div className={styles.header}>
+      <Hamburguer_menu></Hamburguer_menu>
+      <Title_box></Title_box>
+      <div>
+        <Btn_reusable params="Profile"></Btn_reusable>
+        <Btn_reusable params="Search"></Btn_reusable>
+      </div>
+    </div>
+  );
+}
+export default Header;
