9aa1f08f32f50d6a716d3f6291cdba9bcefd15b6
Ebenitez03 <elianenbenitezd04@gmail.com>
2024-12-03 08:48:48 +0100

    test: failed test, need refactor

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Btn_reusable from "./components/btn_reusable/Btn_reusable";
-import Hamburguer_menu from './components/header/hamburguer_menu/Hamburguer_menu';
+import Header from './components/header/Header';
 
 function App() {
   return (
     <>
-      <div>
-        <body>
-        <header>
-          <Hamburguer_menu></Hamburguer_menu>
-          <Btn_reusable params="Profile"></Btn_reusable>
-          <Btn_reusable params='Search'></Btn_reusable>
-        </header>
+      <div className='app'>
+        <Header></Header>
         <div>
 
         </div>
         <footer>
 
         </footer>
-        </body>
       </div>
       
     </>
