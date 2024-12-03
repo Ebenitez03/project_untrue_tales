e5d51984edfcb5d2a926265fb995304c30bd7bd5
Ebenitez03 <elianenbenitezd04@gmail.com>
2024-12-03 16:25:37 +0100

    refactor: new ideas hamburguer menu

--- src/components/header/hamburguer_menu/Hamburguer_menu.tsx
+++ src/components/header/hamburguer_menu/Hamburguer_menu.tsx
@@ -4,6 +4,8 @@ import styles from "./Hamburguer_menu.module.scss";
 function Hamburguer_menu() {
     return(
         <div className={styles.parent}>
+            /* La idea es crear un svg de libro que
+            al clickar se separe en los botones */
             <div className={styles.child}>
                 <Btn_reusable params="books"></Btn_reusable>
                 <Btn_reusable params="parents"></Btn_reusable>
